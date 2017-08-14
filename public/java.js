ac7ce7c37ec93c42ac6b1ab4ef5afc7854a79140
scmilee <drudged@oregonstate.edu>
2017-08-14 01:24:04 -0700

    Update java.js

--- public/java.js
+++ public/java.js
@@ -84,7 +84,7 @@ req.addEventListener('load', function(){
       console.log("error");
   }
 });
-res.send("/insert?" + holder);
+req.send("/insert?" + holder);
 event.preventDefault();                                     //no refreshes!
 });
 //function that well, deletes an entry
